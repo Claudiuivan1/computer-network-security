1116f5a56279092f205d8b3c717407a290bb9e30
Claudiu Ivan <claudiu97@hotmail.com>
2020-10-27 16:27:39 +0100

    AES-128 cypher (4)
    - Add decryption

--- ciphers/AES128.py
+++ ciphers/AES128.py
@@ -34,11 +34,37 @@ class AES128( object ):
             0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16
         ]
         
+        self.rsbox = [
+            0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+            0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+            0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+            0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+            0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+            0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+            0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+            0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+            0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+            0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+            0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+            0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+            0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+            0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+            0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+            0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d
+        ]
+
         self.mix_matrix = [
             0x02, 0x03, 0x01, 0x01,
             0x01, 0x02, 0x03, 0x01,
             0x01, 0x01, 0x02, 0x03,
             0x03, 0x01, 0x01, 0x02
+        ]
+        
+        self.rmix_matrix = [
+            0x0e, 0x0b, 0x0d, 0x09,
+            0x09, 0x0e, 0x0b, 0x0d,
+            0x0d, 0x09, 0x0e, 0x0b,
+            0x0b, 0x0d, 0x09, 0x0e
         ]
         
         self.rCon = [
@@ -57,7 +83,7 @@ class AES128( object ):
         self.w = self.keyExpansion( key )
         
         self.splitted = []
-        self.cyphertext = []
+        self.ciphertext = []
         
                 
     def splitText( self, string ): # Split plaintext in blocks of specified size
@@ -85,6 +111,7 @@ class AES128( object ):
     
         
     def encrypt( self ):
+        ciphertext = []
         for i in range( 0, len( self.splitted ) ):
             if( self.mode == 'ECB' ):
                 block = self.splitted[i]
@@ -94,13 +121,13 @@ class AES128( object ):
                 if( i == 0 ):
                     block = self.xor( block, self.iv )
                 else:
-                    block = self.xor( block, self.cyphertext[i-1] )
+                    block = self.xor( block, ciphertext[i-1] )
                 block = self.encryptBlock( block )
             elif( self.mode == 'CFB' ):
                 if( i == 0 ):
                     block = [] + self.iv
                 else:
-                    block = self.cyphertext[i-1]
+                    block = ciphertext[i-1]
                 block = self.encryptBlock( block )
                 block = self.xor( block, self.splitted[i] )
             elif( self.mode == 'OFB' ):
@@ -121,7 +148,8 @@ class AES128( object ):
                 block = self.xor( block, self.splitted[i] )
             if( self.mode == 'CTR' ):
                 self.iv = [] + temp
-            self.cyphertext.append( block )
+            ciphertext.append( block )
+        self.ciphertext = [] + ciphertext
         
         
     def encryptBlock( self, block ):
@@ -161,15 +189,7 @@ class AES128( object ):
                 block[4], block[9], block[14], block[3], 
                 block[8], block[13], block[2], block[7], 
                 block[12], block[1], block[6], block[11] ]
-        return out 
-        
-    
-    def incrementCounter( self ):
-        i = 15
-        while( int( self.iv[i], 16 ) == 255 ):
-            self.iv[i] = hex( 0 )
-            i-= 1
-        self.iv[i] = hex( int( self.iv[i], 16 ) + 1 )        
+        return out   
         
         
     def mixColumns( self, block ): # Combinazione dei byte con un'operazione lineare, i byte vengono trattati una colonna per volta.
@@ -186,6 +206,98 @@ class AES128( object ):
         out = [0] * 16
         for i in range( 0, 16 ):
             out[i] = self.byteXor( int( block[i], 16 ), int( key[i], 16 ) )
+        return out
+        
+        
+    def decrypt( self ):
+        plaintext = []
+        for i in range( 0, len( self.splitted ) ):
+            if( self.mode == 'ECB' ):
+                block = self.splitted[i]
+                block = self.decryptBlock( block )
+            elif( self.mode == 'CBC' ):
+                block = self.splitted[i]
+                block = self.decryptBlock( block )
+                if( i == 0 ):
+                    block = self.xor( block, self.iv )
+                else:
+                    block = self.xor( block, plaintext[i-1] )
+            elif( self.mode == 'CFB' ):
+                if( i == 0 ):
+                    block = [] + self.iv
+                else:
+                    block = self.ciphertext[i-1]
+                block = self.decryptBlock( block )
+                block = self.xor( block, self.ciphertext[i] )
+            elif( self.mode == 'OFB' ):
+                if( i == 0 ):
+                    block = [] + self.iv
+                else:
+                    block = temp
+                block = self.decryptBlock( block )
+                temp = block
+                block = self.xor( block, self.ciphertext[i] )
+            elif( self.mode == 'CTR' ):
+                if( i == 0 ):
+                    temp = [] + self.iv
+                else:
+                    self.incrementCounter()
+                block = [] + self.iv
+                block = self.decryptBlock( block )
+                block = self.xor( block, self.ciphertext[i] )
+            if( self.mode == 'CTR' ):
+                self.iv = [] + temp
+            plaintext.append( block )
+        self.plaintext = [] + plaintext
+        
+        
+    def decryptBlock( self, block ):
+        key = self.w[0] + self.w[1] + self.w[2] + self.w[3]
+        block = self.addRoundKey( block, key )
+        for i in range( 1, self.nr ):
+            block = self.invSubBytes( block )
+            block = self.invShiftRows( block )
+            block = self.invMixColumns( block )
+            key = self.w[i*4] + self.w[i*4+1] + self.w[i*4+2] + self.w[i*4+3]
+            block = self.addRoundKey( block, key )
+        block = self.invSubBytes( block )
+        block = self.invShiftRows( block )
+        block = list( map( hex, block ) )
+        key = self.w[(i+1)*4] + self.w[(i+1)*4+1] + self.w[(i+1)*4+2] + self.w[(i+1)*4+3]
+        block = self.addRoundKey( block, key )
+        return block
+        
+        
+    def invSubBytes( self, block ): # Sostituzione non lineare di tutti i byte che vengono rimpiazzati secondo una specifica tabella.
+        out = []
+        for i in block:
+            if( len( i ) == 3 ):
+                row = 0
+            else:
+                row = int( i[2], 16 )
+            if( len( i ) == 3 ):
+                col = int( i[2], 16 )
+            else:
+                col = int( i[3], 16 )
+            out.append( self.rsbox[col+16*(row)] )
+        return out
+            
+        
+    def invShiftRows( self, block ): # Spostamento dei byte di un certo numero di posizioni dipendente dalla riga di appartenenza.
+        out = [ block[0], block[1], block[2], block[3], 
+                block[7], block[4], block[5], block[6], 
+                block[10], block[11], block[8], block[9], 
+                block[13], block[14], block[15], block[12] ]
+        return out   
+        
+        
+    def invMixColumns( self, block ): # Combinazione dei byte con un'operazione lineare, i byte vengono trattati una colonna per volta.
+        out = [0] * self.nb * 4
+        for i in range( 0, self.nb ):
+            for j in range ( 0, self.nb ):
+                for k in range( 0, self.nb ):
+                    out[j+i*self.nb] = int( self.byteXor( out[j+i*self.nb], self.polyMult( self.rmix_matrix[k+j*self.nb], block[k+i*self.nb] ) ), 16 )
+                out[j+i*self.nb] = hex( out[j+i*self.nb] )
         return out
         
         
@@ -234,16 +346,7 @@ class AES128( object ):
             out[i-1] = block[i]
         out[3] = temp
         return out
-
- 
-    def xor( self, w1, w2 ):
-        out = []
-        for i in range( 0, len( w1 ) ):
-            out.append( self.byteXor( int( w1[i], 16), int( w2[i], 16) ) )
-        return out
         
-    def byteXor( self, s1, s2 ):
-        return hex( s1 ^ s2 )
         
     def polyMult( self, a, b ):
         p = 0
@@ -257,15 +360,44 @@ class AES128( object ):
             b >>= 1
         return p
         
+       
+    def incrementCounter( self ):
+        i = 15
+        while( int( self.iv[i], 16 ) == 255 ):
+            self.iv[i] = hex( 0 )
+            i-= 1
+        self.iv[i] = hex( int( self.iv[i], 16 ) + 1 )      
+
+ 
+    def xor( self, w1, w2 ):
+        out = []
+        for i in range( 0, len( w1 ) ):
+            out.append( self.byteXor( int( w1[i], 16), int( w2[i], 16) ) )
+        return out
+        
+        
+    def byteXor( self, s1, s2 ):
+        return hex( s1 ^ s2 )
+        
         
     def setPlaintext( self, plaintext ):
         self.splitted = self.splitText( plaintext )
         
         
-    def getCyphertext( self ):
+    def setCiphertext( self, ciphertext ):
+        self.ciphertext = self.splitText( ciphertext )
+        
+        
+    def getCiphertext( self ):
         print( "IV: ", "".join( map( self.convertHex, self.iv ) ) )
         print( "Key: ", "".join( map( self.convertHex, self.key ) ) )
-        print( "Cyphertext: ", "".join( map( self.convertHex, map( "".join, self.cyphertext ) ) ) )
+        print( "Ciphertext: ", "".join( map( self.convertHex, map( "".join, self.ciphertext ) ) ) )
+        
+        
+    def getPlaintext( self ):
+        print( "IV: ", "".join( map( self.convertHex, self.iv ) ) )
+        print( "Key: ", "".join( map( self.convertHex, self.key ) ) )
+        print( "Plaintext: ", "".join( map( self.convertHex, map( "".join, self.splitted ) ) ) )
  
  
     def convertHex( self, str ):
@@ -279,4 +411,8 @@ class AES128( object ):
 a = AES128( '2b7e151628aed2a6abf7158809cf4f3c', 'CTR' )
 a.setPlaintext( '3243f6a8885a308d313198a2e03707343243f6a8885a308d313198a2e0370734' )
 a.encrypt()
-a.getCyphertext()
+a.getCiphertext()
+
+a.setCiphertext( '3243f6a8885a308d313198a2e03707343243f6a8885a308d313198a2e0370734' )
+a.decrypt()
+a.getPlaintext()
