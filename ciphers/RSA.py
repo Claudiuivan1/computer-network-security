a3a3cbd88c0fc678ca8ccbdc355ca3c7c49b804a
Claudiu Ivan <claudiu97@hotmail.com>
2020-11-12 22:18:50 +0100

    RSA (2)
    - Fixes

--- ciphers/RSA.py
+++ ciphers/RSA.py
@@ -1,37 +1,46 @@
 """ RSA class:
         This class gives you the power of RSA encryption standard """
 
-from Crypto.Util.number import bytes_to_long, long_to_bytes
-from Crypto.Random import get_random_bytes 
-import Crypto
-import libnum
-import sys
+from Crypto.Util.number import bytes_to_long, long_to_bytes, getPrime, inverse
 
 class RSA( object ):
-    def __init__( self ):
+    def __init__( self, length ):
         self.plaintext = ''
         self.ciphertext = ''
         
-        self.bits = 60
-        self.e = 65537
+        self.length = length
         
-        self.p = Crypto.Util.number.getPrime( self.bits, randfunc=get_random_bytes )
-        self.q = Crypto.Util.number.getPrime( self.bits, randfunc=get_random_bytes )
-        self.n = self.p*self.q
-        self.phi = ( self.p-1 )*( self.q-1 )
-        self.d = libnum.invmod( self.e, self.phi )
+        self.p = getPrime( self.length )
+        self.q = getPrime( self.length )
+        self.n = self.p * self.q
+        self.phi = ( self.p - 1 ) * ( self.q - 1 )
+        
+        self.e = 65537
+        self.d = inverse( self.e, self.phi )
+        
         
     def encrypt( self ):
         self.ciphertext = pow( self.plaintext, self.e, self.n )
         
+        
     def decrypt( self ):
-        self.plaintext = long_to_bytes( pow( self.ciphertext, self.d, self.n ) )
+        self.plaintext = pow( self.ciphertext, self.d, self.n )
+        
         
     def setPlaintext( self, plaintext ):
-        self.plaintext = bytes_to_long( plaintext.encode( 'utf-8' ) )
+        if( len( plaintext ) < self.n ):
+            self.plaintext = bytes_to_long( plaintext.encode( 'utf-8' ) )
+        else:
+            print( "Plaintext is too long, try to use hybrid encryption" )
+        
         
     def getPlaintext( self ):
-        return self.plaintext
+        return "Plaintext: " + long_to_bytes( self.plaintext ).decode("utf-8")
+        
         
     def getCiphertext( self ):
-        return self.ciphertext
+        return "Ciphertext: " + str( self.ciphertext )
+        
+        
+    def getKeys( self ):
+        return "Public key: " + str( self.e ) + "\nPrivate key: " + str( self.d )
